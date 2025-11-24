2ce33d903add505446d75751faa6f1a1f9fc4708
Morgan Roman <morgan@turnkey.io>
2025-11-24 14:33:24 -0500

    better deploy params

--- script/BaseDeployReimbursableGasStationUSDC.s.sol
+++ script/BaseDeployReimbursableGasStationUSDC.s.sol
@@ -11,8 +11,8 @@ contract BaseDeployReimbursableGasStationUSDC is Script {
     address private constant REIMBURSEMENT_ERC20 = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
     address private constant TK_GAS_DELEGATE = 0x000066a00056CD44008768E2aF00696e19A30084;
     uint16 private constant GAS_FEE_BASIS_POINTS = 100; // 1% (100 basis points)
-    uint256 private constant BASE_GAS_FEE_ERC20 = 250000; // Base gas fee: 25 cents in USDC (0.25 * 10^6)
-    uint256 private constant MAX_GAS_LIMIT_ERC20 = 10_000_000_000_000; // Max gas limit in ERC20 tokens (10k USDC with 6 decimals)
+    uint256 private constant BASE_GAS_FEE_ERC20 = 10_000; // Base gas fee: 1 cent in USDC (0.01 * 10^6)
+    uint256 private constant MAX_GAS_LIMIT_ERC20 = 10_000_000; // Max gas limit: 10 dollars in USDC (10 * 10^6)
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
